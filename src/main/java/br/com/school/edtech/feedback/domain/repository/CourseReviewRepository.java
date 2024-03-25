b2774a2c9f80ae0a3e0009a7af585a50cbe3d060
jeronima <florianojeronima@gmail.com>
2024-03-25 20:43:57 -0300

    feat: Adding method for course review search

--- src/main/java/br/com/school/edtech/feedback/domain/repository/CourseReviewRepository.java
+++ src/main/java/br/com/school/edtech/feedback/domain/repository/CourseReviewRepository.java
@@ -5,9 +5,11 @@ import br.com.school.edtech.course.domain.model.CourseId;
 import br.com.school.edtech.feedback.domain.model.CourseReview;
 import br.com.school.edtech.feedback.domain.model.CourseReviewId;
 import br.com.school.edtech.user.domain.model.UserId;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -15,4 +17,7 @@ public interface CourseReviewRepository extends JpaRepository<CourseReview, Cour
     JpaSpecificationExecutor<Course> {
 
   Optional<CourseReview> findByCourseIdAndUserId(CourseId courseId, UserId userId);
+
+  @Query("SELECT cr FROM CourseReview cr WHERE cr.course IN :courses")
+  List<CourseReview> findReviewsByCourses(List<Course> courses);
 }
