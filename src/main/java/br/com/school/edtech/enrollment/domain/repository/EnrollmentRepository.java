1922e5bf81c76decfbcf655dede680e747de1083
jeronima <florianojeronima@gmail.com>
2024-03-25 20:45:37 -0300

    fix: Removing an unused method

--- src/main/java/br/com/school/edtech/enrollment/domain/repository/EnrollmentRepository.java
+++ src/main/java/br/com/school/edtech/enrollment/domain/repository/EnrollmentRepository.java
@@ -1,12 +1,9 @@
 package br.com.school.edtech.enrollment.domain.repository;
 
 import br.com.school.edtech.course.domain.model.Course;
-import br.com.school.edtech.course.domain.model.CourseId;
 import br.com.school.edtech.enrollment.domain.model.Enrollment;
 import br.com.school.edtech.enrollment.domain.model.EnrollmentId;
-import br.com.school.edtech.feedback.domain.model.CourseReviewId;
 import br.com.school.edtech.user.domain.model.User;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -15,7 +12,4 @@ import org.springframework.stereotype.Repository;
 public interface EnrollmentRepository extends JpaRepository<Enrollment, EnrollmentId> {
 
   Optional<Enrollment> findByCourseAndUser(Course course, User user);
-
-  List<Enrollment> findAllByCourseId(List<CourseId> courseIds);
-
 }
