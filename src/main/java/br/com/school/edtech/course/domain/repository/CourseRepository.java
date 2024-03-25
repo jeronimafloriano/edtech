6b72ab48525c7e3c3d68d5519fc04633768b43f2
jeronima <florianojeronima@gmail.com>
2024-03-25 20:34:08 -0300

    feat: Added method in CourseRepository class to return Courses with more than four enrollments

--- src/main/java/br/com/school/edtech/course/domain/repository/CourseRepository.java
+++ src/main/java/br/com/school/edtech/course/domain/repository/CourseRepository.java
@@ -2,9 +2,11 @@ package br.com.school.edtech.course.domain.repository;
 
 import br.com.school.edtech.course.domain.model.Course;
 import br.com.school.edtech.course.domain.model.CourseId;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,4 +14,7 @@ public interface CourseRepository extends JpaRepository<Course, CourseId>,
     JpaSpecificationExecutor<Course> {
 
   Optional<Course> findByCode(String code);
+
+  @Query("SELECT c FROM Course c WHERE SIZE(c.enrollments) > 4")
+  List<Course> findCoursesWithMoreThanFourEnrollments();
 }
