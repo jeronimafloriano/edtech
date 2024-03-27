a2c37d4b12f04b5f917e1377864b24d701f687cd
jeronima <florianojeronima@gmail.com>
2024-03-27 08:52:22 -0300

    fix: Removing unused imports

--- src/main/java/br/com/school/edtech/shared/finder/CourseFinder.java
+++ src/main/java/br/com/school/edtech/shared/finder/CourseFinder.java
@@ -1,7 +1,6 @@
 package br.com.school.edtech.shared.finder;
 
 import br.com.school.edtech.course.domain.model.Course;
-import br.com.school.edtech.user.domain.model.User;
 import java.util.List;
 import java.util.UUID;
 
--- src/main/java/br/com/school/edtech/user/domain/repository/UserRepository.java
+++ src/main/java/br/com/school/edtech/user/domain/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package br.com.school.edtech.user.domain.repository;
 
-import br.com.school.edtech.shared.model.DomainEntityId;
 import br.com.school.edtech.user.domain.model.Email;
 import br.com.school.edtech.user.domain.model.User;
 import br.com.school.edtech.user.domain.model.UserId;
